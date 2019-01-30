01d50bdedf384b23b2dc385f16d8ce3df8beefce
Brendan <yang.b@husky.neu.edu>
2019-01-30 00:34:27 -0500

    memory game minus good style

--- assets/js/memory-game.jsx
+++ assets/js/memory-game.jsx
@@ -23,53 +23,47 @@ class Starter extends React.Component {
     this.setState( _.assign({}, this.state, {board: cards}));
   }
 
-  updateBoard(board, newCard, index) {
+  flip(index, board) {
+    let cardToFlip = board[index];
+    let newCard = new Card(cardToFlip.value, !cardToFlip.hidden);
     let newBoard = board.slice();
     newBoard.splice(index, 1, newCard);
     return newBoard;
   }
 
-  flip(index) {
-    let cardToFlip = this.state.board[index];
-    let newCard = new Card(cardToFlip.value, !cardToFlip.hidden);
-    let newBoard = this.updateBoard(this.state.board, newCard, index);
-    this.setState( _.assign({}, this.state, { board: newBoard }));
-  }
-
-  checkMatch(currentCard, currIndex) {
+  checkMatch(currIndex) {
+    let newBoard = this.flip(currIndex, this.state.board);
+    let currentCard = newBoard[currIndex];
     let known = this.state.revealed;
     if (known.length == 0) {
-	    console.log("block1");
-       this.setState(_.assign({}, this.state, {revealed: [currentCard, currIndex]}));
+       this.setState(_.assign({}, this.state,
+	       {board: newBoard, revealed: [currentCard, currIndex]}));
     }
     else if (known[0].value == currentCard.value
 	    && currIndex != known[1]) {
-       console.log("block2");
-       let score2 = Math.max(0, this.state.score + 100 - 10 * this.state.penalty);
+       let score2 = this.state.score + Math.max(0, 100 - 10*this.state.penalty);
        let newCard1 = new Card(currentCard.value, null);
        let newCard2 = new Card(known[0].value, null);
-       let newBoard = this.updateBoard(this.updateBoard(
-	       this.state.board, newCard1, currIndex), newCard2, known[1]);
+       newBoard.splice(currIndex, 1, newCard1);
+       newBoard.splice(known[1], 1, newCard2);
        this.setState(_.assign({}, this.state,
 	       {revealed: [], score: score2, board: newBoard, penalty: 0}));
     }
-    else {
+    else if (known[0].value != currentCard.value) {
        let penalty2 = this.state.penalty + 1;
-	    //TODO maybe wait a lil
-       this.flip(currIndex);
-       this.flip(known[1]);
-       this.setState(_.assign({}, this.state, {penalty: penalty2, revealed: []}));
+       this.setState(_.assign({}, this.state,
+	       {penalty: penalty2, revealed: [], board: newBoard}));
+       setTimeout(() => {
+         let newBoard2 = this.flip(currIndex, this.state.board);
+         let newBoard3 = this.flip(known[1], newBoard2);
+         this.setState(_.assign({}, this.state, {board: newBoard3}));
+       },1000)
     }
-  }
-
-  move(card, cardIndex) {
-    this.flip(cardIndex);
-    this.checkMatch(card, cardIndex);
   }
 
   render() {
     let cards = _.map(this.state.board, (card, ii) => {
-      return <RenderedCard card={card} move={this.move.bind(this)} key={ii} index={ii} />;
+      return <RenderedCard card={card}  checkMatch={this.checkMatch.bind(this)} key={ii} index={ii} />;
     });
     return (
       <div>
@@ -93,8 +87,12 @@ class Card {
 
 function RenderedCard(props) {
   let card = props.card;
-	//TODO SWITCH statement
-  if (card.hidden == null) return <li class="revealed">{card.value}</li>;
-  if (card.hidden == true) return <li class="hidden" onClick={() => props.move(card, props.index)}>&nbsp;</li>;
-  else return <li class="revealed" onClick={() => props.move(card, props.index)}>{card.value}</li>
+  switch (card.hidden) {
+    case null:
+      return <li class="revealed">{card.value}</li>;
+    case true:
+      return <li class="hidden" onClick={() => props.checkMatch(props.index)}>&nbsp;</li>;
+    default:
+      return <li class="revealed" onClick={() => props.checkMatch(props.index)}>{card.value}</li>
+  }
 }
